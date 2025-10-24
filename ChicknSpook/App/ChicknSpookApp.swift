77ebe65927dd0ad443d9fb7650b3b38e9669c63f
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-10-24 10:59:16 +0300

    init

---
+++ ChicknSpook/App/ChicknSpookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChicknSpookApp.swift
+//  ChicknSpook
+//
+//  Created by Serhii Babchuk on 24.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChicknSpookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CSLaunchView()
+        }
+    }
+}
